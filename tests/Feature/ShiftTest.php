4d6f3bcc39d5b4956d011017d9771d10dd00a544
Yakubu Alhassan <yaqoubdramani@gmail.com>
2023-12-02 06:51:20 +0000

    test: add test for creating a new Shift

--- tests/Feature/ShiftTest.php
+++ tests/Feature/ShiftTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use App\Models\Shift;
+use App\Models\Worker;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -24,6 +25,26 @@ class ShiftTest extends TestCase
             ->assertJsonCount(5, 'data')
             ->assertJsonStructure([
                 'data'
+            ]);
+    }
+
+    /**
+     * @test
+     */
+    public function testItCreatesShiftForWorker()
+    {
+        $worker = Worker::factory()->create();
+
+        $input = [
+            'worker_id' => $worker->id,
+            'start_time' => '2023-04-04 16:00:00',
+            'end_time' => '2023-04-04 24:00:00'
+        ];
+
+        $response = $this->json('POST', route('shifts.store'), $input);
+        $response->assertStatus(201)
+            ->assertJsonFragment([
+                'worker_id' => $input['worker_id']
             ]);
     }
 }
