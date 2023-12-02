c1efd38a4423947664a411ddca1c2d2edbda565e
Yakubu Alhassan <yaqoubdramani@gmail.com>
2023-12-02 05:59:52 +0000

    api route: add shifts endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ShiftController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WorkerController;
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('workers', WorkerController::class);
+Route::apiResource('shifts', ShiftController::class);
